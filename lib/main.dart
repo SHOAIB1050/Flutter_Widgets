49eed5d8309bdb4f27f2de77c219fbf38f05b443
Shoaib Farooq <m.shoaib1050@gmail.com>
2023-10-01 11:08:11 +0500

    Assert folder How add images from images folder in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.pink,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Scaffold Wifget',),
-      ),
-      body: Center(child: Image.network('https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_640.jpg'),),
-    ),
+        backgroundColor: Colors.pink,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'Scaffold Wifget',
+          ),
+        ),
+        body: Center(child: Image.asset('images/123.jpeg'))),
   ));
 }
